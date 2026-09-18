b0d1b3015cc9585d922faf9d13b0f304e66d0cd9
Jose Perez <joseperezmc0701@hotmail.com>
2026-09-18 14:21:43 -0500

    chore(studio): fijar appId del deploy para evitar prompt

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -7,10 +7,10 @@ export default defineCliConfig({
   },
   deployment: {
     /**
-     * Enable auto-updates for studios.
-     * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
+     * Auto-updates for studios.
      */
     autoUpdates: true,
+    appId: 'hlltvjvtl8qgahdgcff3kk06',
   },
   typegen: {
     enabled: true,
