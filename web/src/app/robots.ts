b767ec55fc114c76fd4704c290cce5252a6404dd
Jose Perez <joseperezmc0701@hotmail.com>
2026-09-18 13:18:22 -0500

    fix(robots): quitar doble barra en URL del sitemap

--- web/src/app/robots.ts
+++ web/src/app/robots.ts
@@ -8,6 +8,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: ["/api/"],
     },
-    sitemap: `${siteUrl}/sitemap.xml`,
+    sitemap: `${siteUrl.replace(/\/$/, "")}/sitemap.xml`,
   };
 }
